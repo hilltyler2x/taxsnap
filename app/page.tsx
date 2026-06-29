b13b64e039fa60b029a89fb4b7672f70e580b664
Tyler Hill <hill.tyler2x@gmail.com>
2026-06-29 16:17:34 -0400

    redirect to landing

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,5 @@ import { authOptions } from "@/lib/auth"
 export default async function Home() {
   const session = await getServerSession(authOptions)
   if (session) redirect("/dashboard")
-  else redirect("/login")
+  else redirect("/landing")
 }
