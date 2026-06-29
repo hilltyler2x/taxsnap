3793ba96ffab9383044cffcbbd5d9d88800e6d19
Tyler Hill <hill.tyler2x@gmail.com>
2026-06-29 16:55:20 -0400

    force nextauth url

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
 
+process.env.NEXTAUTH_URL = process.env.NEXTAUTH_URL || "https://taxsnap-jet.vercel.app"
+
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
